62bc26333b6a7c822f70af733f3baf7f0b0bc1ab
Daffnel <3ggapp24.andrda@folkuniversitetet.nu>
2025-05-15 19:50:34 +0200

    Initial Commit

---
+++ SwiftUIFireBaseBootCamp/SwiftUIFireBaseBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFireBaseBootCampApp.swift
+//  SwiftUIFireBaseBootCamp
+//
+//  Created by Daniel A on 2025-05-15.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFireBaseBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
